6f3beff51f35cb15805e700ffebf94afaf8fd8bd
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-09-04 03:10:24 +0100

    Implement support for databases
    We have a custom orm.Model abstraction that lets us skip entering in the
    database and metadata for every model. We do still need to specify the
    __tablename__ attribute manually, though. Maybe using Comet it should be
    called __table__ to make it shorter to type

---
+++ comet/database.py
@@ -0,0 +1,36 @@
+import typing
+
+from databases import DatabaseURL, Database as DatabaseBackend
+from sqlalchemy import MetaData
+import orm, orm.models
+
+
+class Database:
+    def __init__(self, url: DatabaseURL):
+        url = DatabaseURL(url)
+
+        if url.scheme == "postgres":
+            # The default postgres backend for databases does not return
+            # RowProxy objects, unlike all the other backends.
+            # Therefore, we use aiopg so that we have dialect-agnostic results.
+            url = url.replace(scheme="postgres+aiopg")
+
+        self.database = DatabaseBackend(url)
+        self.metadata = MetaData()
+
+        class DatabaseModelMetaclass(orm.models.ModelMetaclass):
+            def __new__(
+                cls: type, name: str, bases: typing.Sequence[type], attrs: dict,
+            ) -> type:
+                attrs["__database__"] = self.database
+                attrs["__metadata__"] = self.metadata
+
+                return super(DatabaseModelMetaclass, cls).__new__(
+                    cls, name, bases, attrs
+                )
+
+        class DatabaseModel(orm.Model, metaclass=DatabaseModelMetaclass):
+            __abstract__ = True
+
+        self.Model = DatabaseModel
+
