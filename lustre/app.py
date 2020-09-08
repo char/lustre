c7b792c9c116c1fd98d84e5a44eb235f72829f60
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-09-08 03:01:49 +0100

    Implement an authentication subsystem (WIP: requires sessions)

--- lustre/app.py
+++ lustre/app.py
@@ -9,14 +9,26 @@ from .global_state import GlobalStateMiddleware
 from .config import ConfigAppMixin
 from .database import DatabaseAppMixin
 from .precomputation import PrecomputationAppMixin
+from .auth import AuthAppMixin
 
 
-class Lustre(Starlette, ConfigAppMixin, DatabaseAppMixin, PrecomputationAppMixin):
+class Lustre(
+    Starlette,
+    ConfigAppMixin,
+    DatabaseAppMixin,
+    PrecomputationAppMixin,
+    AuthAppMixin,
+):
     def __init__(self, config_files=(".env", ".env.secrets")):
         Starlette.__init__(self, middleware=[Middleware(GlobalStateMiddleware)])
         ConfigAppMixin.__init__(self, config_files)
         DatabaseAppMixin.__init__(self)
         PrecomputationAppMixin.__init__(self)
+        AuthAppMixin.__init__(self)
+
+    def append_middleware(self, middleware_class: type, **options: typing.Any) -> None:
+        self.user_middleware.append(Middleware(middleware_class, **options))
+        self.middleware_stack = self.build_middleware_stack()
 
     def add_static_folder(
         self, directory: str, path: str = None, name: str = None, **kwargs
---
+++ lustre/auth.py
@@ -0,0 +1,77 @@
+import typing
+
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.authentication import (
+    AuthCredentials,
+    AuthenticationBackend,
+    AuthenticationError,
+    BaseUser,
+)
+
+from .global_state import current_request
+
+
+class AuthUser(BaseUser):
+    """
+    An authentication object.
+
+    Children must define:
+        - (class method) create_from_token
+        - to_token
+        - (property) display_name
+
+    And may define:
+        - (async) full
+    """
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    def to_token(self) -> str:
+        raise NotImplementedError(
+            "to_token is not implemented for '{}'".format(self.__class__.__name__)
+        )
+
+    @classmethod
+    def create_from_token(cls: type, token: str):
+        raise NotImplementedError(
+            "create_from_token is not implemented for '{}'".format(cls.__name__)
+        )
+
+    async def full(self):
+        raise NotImplementedError(
+            "full is not implemented for '{}'".format(self.__class__.__name__)
+        )
+
+
+class LustreAuthBackend(AuthenticationBackend):
+    def __init__(self, user_type: typing.Type[AuthUser]):
+        assert issubclass(user_type, AuthUser), f"{user_type.__name__} is not an AuthUser!""
+        self.user_type = user_type
+
+    def log_in(self, user: AuthUser):
+        assert isinstance(auth_obj, self.user_type), f"The given user is not a(n) {self.user_type.__name__}"
+        current_request().session["lustre_auth_token"] = user.to_token()
+
+    def log_out(self):
+        session = current_request().session
+        if "lustre_auth_token" in session:
+            del session["lustre_auth_token"]
+
+    def get_auth_object(self, token):
+        return self.user_type.create_from_token(token)
+
+    async def authenticate(self, request):
+        if auth_token := request.session.get("lustre_auth_token"):
+            return (
+                AuthCredentials(["authenticated"]),
+                self.get_auth_object(auth_token)
+            )
+
+
+class AuthAppMixin:
+    def setup_auth(self, user_type: typing.Type[AuthUser]):
+        self.auth = LustreAuthBackend(user_type)
+        self.append_middleware(AuthenticationMiddleware, backend=self.auth)
+        return user_type # Return the user type so that we can use @app.setup_auth as a decorator
